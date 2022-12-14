c0e5b4945ea2e15c53db080401f4e9c91e942363
Alberto Valdes <anvaldes@uc.cl>
2022-12-13 23:08:48 -0300

    Updating the Virtual Environments Contents

---
+++ Data_Science_Contents/Virtual_Environments/Python_File/Python_Example.py
@@ -0,0 +1,15 @@
+import torch
+import sklearn
+import pycaret
+import scipy
+import numpy as np
+import pandas as pd
+import seaborn as snb
+import matplotlib.pyplot as plt
+
+
+arr = np.array([1, 2, 3])
+df = pd.DataFrame({'X': [1, 2, 3], 'Y': [4, 5, 6]})
+
+
+print(df['X'])
