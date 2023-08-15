e48596fa54097b7f67cd94c18ff423592b392fa5
Beto Valdes <anvaldes@uc.cl>
2023-08-15 18:06:02 -0400

    Updating Contents of DS

---
+++ Data_Science_Contents/General_Topics/18.Name_Main/file_1.py
@@ -0,0 +1,12 @@
+def step1():
+   print("Executing the first step...")
+
+def step2():
+   print("Executing the second step...")
+   
+def step3():
+   print("Executing the third step...")
+
+step1()
+step2()
+step3()
---
+++ Data_Science_Contents/General_Topics/18.Name_Main/file_2.py
@@ -0,0 +1,14 @@
+def step1():
+   print("Executing the first step...")
+
+def step2():
+   print("Executing the second step...")
+   
+def step3():
+   print("Executing the third step...")
+
+if __name__ == '__main__':
+    
+    step1()
+    step2()
+    step3()
